ade51e1b7cf60817563de386005881fb5c574c21
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-03-28 17:01:53 +0530

    add functionality by js in get started button that pop up the contact form

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 let burgerButton = document.getElementById("burger-btn");
 let navMenu = document.getElementById("nav-menu");
 
-burgerButton.addEventListener("click", function(){
+burgerButton.addEventListener("click", function () {
     navMenu.classList.toggle("open");
 });
 
 let cards = document.querySelectorAll(".price-card");
 let cardButton = document.querySelectorAll(".card-button");
 
-cards.forEach(function(card){    
-    card.addEventListener("click", function(){
-        cards.forEach(function(c){
-        c.classList.remove("selected-card");
-    })
-    card.classList.add("selected-card");
+cards.forEach(function (card) {
+    card.addEventListener("click", function () {
+        cards.forEach(function (c) {
+            c.classList.remove("selected-card");
+        })
+        card.classList.add("selected-card");
     })
 });
-cardButton.forEach(function(btn){
-    btn.addEventListener("click", function(e){
+cardButton.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
         e.stopPropagation();
+    });
+});
+
+let modal = document.getElementById("modal-overlay");
+let getStartedButton = document.querySelectorAll(".get-started-btn");
+
+getStartedButton.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        modal.classList.toggle("get-started")
     });
 });
